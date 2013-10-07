2e381e50d0549beda235cfa9d9fc91debad618aa
mcolombo <mcolombo@enter.it>
2013-10-07 16:33:07 +0200

    Adding mysql

--- modules/mylvmbackup/files/user_setup.sql
+++
@@ -1,3 +0,0 @@
-CREATE USER 'mylvmbackup'@'localhost' IDENTIFIED BY 'notagoodpassword';
-GRANT RELOAD, REPLICATION CLIENT ON *.* TO 'mylvmbackup'@'localhost';
-GRANT SELECT ON mysql.* TO 'mylvmbackup'@'localhost';
--- modules/puppetlabs_apt/README.markdown
+++
@@ -1,5 +0,0 @@
-puppet-puppetlabs\_apt
-=====================
-
-This module installs the puppetlabs community apt repository.
-
