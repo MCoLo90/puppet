9f3e3bc4caac1e9230c95e62db186eaae1a43e8a
mcolombo <mcolombo@enter.it>
2013-10-07 10:23:41 +0200

    Adding mysql

--- modules/mysql/manifests/init.pp
+++ modules/mysql/manifests/init.pp
@@ -1,20 +1,3 @@
 class { '::mysql::server':
   override_options => { 'mysqld' => { 'max_connections' => '1024' } }
 }
-
-class mysql {
-package { 'mysql-server':
- ensure => installed,
-}
-
-service { 'mysql-server':
- ensure => running,
- enable => true,	
- require => Package['mysql-server'],
-}
-
-
-
-
-}
-
